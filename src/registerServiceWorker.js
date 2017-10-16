6cf141c75be02cf9352af6bb054cc2ddf21925cf
Marco Antonio Dominguez <madmzr@gmail.com>
2017-10-16 12:06:21 -0500

    Update serviceWorker
    ## What? 
    Update register method
    
    ## Why?
    It has some readability issues

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -9,36 +9,34 @@
 // This link also includes instructions on opting out of this behavior.
 
 export default function register() {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-      navigator.serviceWorker
-        .register(swUrl)
-        .then(registration => {
-          registration.onupdatefound = () => {
-            const installingWorker = registration.installing;
-            installingWorker.onstatechange = () => {
-              if (installingWorker.state === 'installed') {
-                if (navigator.serviceWorker.controller) {
-                  // At this point, the old content will have been purged and
-                  // the fresh content will have been added to the cache.
-                  // It's the perfect time to display a "New content is
-                  // available; please refresh." message in your web app.
-                  console.log('New content is available; please refresh.');
-                } else {
-                  // At this point, everything has been precached.
-                  // It's the perfect time to display a
-                  // "Content is cached for offline use." message.
-                  console.log('Content is cached for offline use.');
-                }
-              }
-            };
-          };
-        })
-        .catch(error => {
-          console.error('Error during service worker registration:', error);
-        });
-    });
+  if (process.env.NODE_ENV !== 'production' || !('serviceWorker' in navigator)) return;
+
+  const [swUrl, sWorker] = [`${process.env.PUBLIC_URL}/service-worker.js`, navigator.serviceWorker];
+
+  window.addEventListener('load', () => {
+    sWorker
+      .register(swUrl)
+      .then(registration => {
+        registration.onupdatefound = () => {
+          const installingWorker = registration.installing;
+
+          installingWorker.onstatechange = () => {
+            if (installingWorker.state !== 'installed') return;
+            let msg = sWorker.controller ? 'New content is available; please refresh.' : 'Content is cached for offline use.';
+            // At this point:
+            // - The old content will have been purged and
+            // the fresh content will have been added to the cache.
+            // It's the perfect time to display a "New content is
+            // available; please refresh." message in your web app.
+            // - Everything has been precached.
+            // It's the perfect time to display a
+            // "Content is cached for offline use." message.
+
+            console.log(msg);
+          }
+        };
+      })
+      .catch(error => console.error('Error during service worker registration:', error));
   }
 }
 
